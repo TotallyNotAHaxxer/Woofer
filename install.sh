332acef45f43879cd2158003ca1358956f647267
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-09-28 01:35:56 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo 'installing'
+
+sudo apt-get update 
+pip3 install tabulate 
+pip3 install scapy 
+pip install scapy 
+pip install tabulate 
+gem install colorize 
+gem install packetgen 
+sudo gem install packetgen 
+sudo gem install colorize
+apt install libpcap-dev
+sudo apt-get install libpcap-dev
+apt-get install libpcap-dev
+
+echo 'have fun'
+
+sudo python3 main.py 
